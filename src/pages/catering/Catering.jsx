495c55c0f1ccab94d6c08f5f4136e6ab3de1f93d
sergioforwardslash <tequetin9@gmail.com>
2023-02-06 13:25:49 -0800

    Footer commit

--- src/pages/catering/Catering.jsx
+++ src/pages/catering/Catering.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './catering.css'
 
+import images from '../../assets/images'
+
 const Catering = () => {
   return (
-    <div className='catering section-padding'>
-      <div className='catering-info'>
+    <div className='catering section-padding' style={{backgroundImage: `url(${images.background})`, backgroundSize: '100%'}}>
+      <div className='catering-info' style={{backgroundImage: `url(${images.catering})`, backgroundSize: '100%'}}>
         <h1>Catering</h1>
         <p className='p__opensans'>Book us for you next event!</p>
         <a href='/'><button>Contact</button></a>
