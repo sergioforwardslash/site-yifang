96bb5acc9dab5fb52c0b8f7d5f659cb3a478db8f
Idannny <dannyivan6@outlook.com>
2023-03-04 17:21:20 -0800

    instafix

--- src/components/instagramfeed/InstagramFeed.jsx
+++ src/components/instagramfeed/InstagramFeed.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-import "instagramfeed.css"
+import React, { useState, useEffect, useRef } from "react";
+import "./instagramfeed.css";
+import axios from "axios";
+
+const InstagramFeed = ({ token, ...props }) => {
+  const [feeds, setFeedsData] = useState([]);
+  //use useRef to store the latest value of the prop without firing the effect
+  const tokenProp = useRef(token);
+  tokenProp.current = token;
+
+  useEffect(() => {
+    // this is to avoid memory leaks
+    const abortController = new AbortController();
+
+    async function fetchInstagramPost() {
+      try {
+        axios
+          .get(
+            `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${props.limit}&access_token=${props.token}`
+          )
+          .then((resp) => {
+            setFeedsData(resp.data.data);
+          });
+      } catch (err) {
+        console.log("error", err);
+      }
+    }
+
+    fetchInstagramPost();
+
+    return () => {
+      // cancel pending fetch request on component unmount
+      abortController.abort();
+    };
+  }, [props.limit]);
 
 
-export const InstagramFeed = () => {
-  return (
-    <div>InstagramFeed</div>
-  )
+    return (
+        <div className="container">
+            {feeds.map((feed) => (
+                <InstagramFeed key={feed.id} feed={feed} />
+            ))}
+        </div>
+    );
 }
+
+export default InstagramFeed;
