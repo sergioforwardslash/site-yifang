c18b8625e6553e955a8317abbed16b861cbbe8fb
Idannny <dannyivan6@outlook.com>
2023-03-13 15:32:52 -0700

    the katchup catering and insta --cell phone adaptability next as

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import catering from '../assets/catering.png'
 import fruitea from '../assets/fruitea.png'
 import fruiteaGrape from '../assets/fruiteaGrape.png'
@@ -24,50 +23,3 @@ export default {
     yifangWhite,
     menutea,
 }
-=======
-import catering from "../assets/catering.png";
-import fruitea from "../assets/fruitea.png";
-import fruiteaGrape from "../assets/fruiteaGrape.png";
-import fruiteaLime from "../assets/fruiteaLime.png";
-import storeFront from "../assets/storeFront.png";
-import threedrink from "../assets/threedrink.png";
-import yiFangPineapple from "../assets/yiFangPineapple.png";
-import background from "../assets/background.jpg";
-import yifangWhite from "../assets/yifanglogo-white.webp";
-import menutea from "../assets/menutea.png";
-import gingerlemontea from "../assets/gingerlemontea.jpg";
-import luyegingertea from "../assets/luyegingertea.jpg";
-import gingerlatte from "../assets/gingerlatte.jpg";
-import pearlgingerlatte from "../assets/pearlgingerlatte.jpg";
-import gingerblacktealatte from "../assets/gingerblacktealatte.jpg";
-import puchong from "../assets/puchonggreentea.jpg";
-import luguoolong from "../assets/luguoolong.jpg";
-import sunmoon from "../assets/sunmoon.jpg";
-import songboling from "../assets/songboling.jpg";
-import saltycreamblacktea from "../assets/saltycreamblacktea.jpg";
-import saltycreampouchong from "../assets/saltycreampouchong.jpg";
-
-export default {
-  catering,
-  fruitea,
-  fruiteaGrape,
-  fruiteaLime,
-  storeFront,
-  threedrink,
-  yiFangPineapple,
-  background,
-  yifangWhite,
-  menutea,
-  gingerlemontea,
-  luyegingertea,
-  gingerlatte,
-  pearlgingerlatte,
-  gingerblacktealatte,
-  puchong,
-  luguoolong,
-  sunmoon,
-  songboling,
-  saltycreamblacktea,
-  saltycreampouchong,
-};
->>>>>>> master
