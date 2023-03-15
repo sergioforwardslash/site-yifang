a3c7d41ae29ab18381e3dd5ce98c3879d4218cb4
sergioforwardslash <tequetin9@gmail.com>
2023-03-15 15:16:50 -0700

    fixes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import catering from '../assets/catering.png'
-import fruitea from '../assets/fruitea.png'
-import fruiteaGrape from '../assets/fruiteaGrape.png'
-import fruiteaLime from '../assets/fruiteaLime.png'
-import storeFront from '../assets/storeFront.png'
-import teas from '../assets/teas.jpg'
-import threedrink from '../assets/threedrink.png'
-import yiFangPineapple from '../assets/yiFangPineapple.png'
-import background from '../assets/background.jpg'
-import yifangWhite from '../assets/yifanglogo-white.webp'
-import menutea from '../assets/menutea.png'
-
-export default {
-    catering,
-    fruitea,
-    fruiteaGrape,
-    fruiteaLime,
-    storeFront,
-    teas,
-    threedrink,
-    yiFangPineapple,
-    background,
-    yifangWhite,
-    menutea,
-}
-=======
 import catering from "../assets/catering.png";
 import fruitea from "../assets/fruitea.png";
 import fruiteaGrape from "../assets/fruiteaGrape.png";
@@ -70,4 +43,3 @@ export default {
   saltycreamblacktea,
   saltycreampouchong,
 };
->>>>>>> master
