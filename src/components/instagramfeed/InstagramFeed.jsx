d6f369c3ef1878929735537d658d0a10e06af028
Idannny <dannyivan6@outlook.com>
2023-03-27 17:21:49 -0700

    edit to display textboxes

--- src/components/instagramfeed/InstagramFeed.jsx
+++ src/components/instagramfeed/InstagramFeed.jsx
@@ -1,54 +1,3 @@
-// import React, { useState, useEffect, useRef } from "react";
-// import "./instagramfeed.css";
-// import Feed from './Feed'
-// import axios from "axios";
-
-// //need to fix and put
-// const testToken = 'IGQVJXYTRQTVM0SHRhaXVjYnRFMjVmenhVak55dmI4eXBsdkNfLTVFV3BvU2RkYmxKRGdtR1hfUVlUQ2UtenpaYmp0ZAFRwU2daNW9CNUxvMjR5RnFFTzlmckdQeWpZALW9RMkFEdktqS1JwdDlmQ18tNQZDZD'
-
-// const InstagramFeed = ({ token, ...props }) => {
-//   const [feeds, setFeedsData] = useState([]);
-//   //use useRef to store the latest value of the prop without firing the effect
-//   const tokenProp = useRef(token);
-//   tokenProp.current = token;
-
-//   useEffect(() => {
-//     // this is to avoid memory leaks
-//     const abortController = new AbortController();
-
-//     async function fetchInstagramPost() {
-//       try {
-//         axios
-        
-//           .get(
-//             `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${props.limit}&access_token=${testToken}`
-//           )
-//           .then((resp) => {
-//             setFeedsData(resp.data.data);
-//           });
-//       } catch (err) {
-//         console.log("error", err);
-//       }
-//     }
-
-//     fetchInstagramPost();
-
-//     return () => {
-//       // cancel pending fetch request on component unmount
-//       abortController.abort();
-//     };
-//   }, [props.limit]);
-
-//     return (
-//         <div className="container">
-//             {feeds.map((feed) => (
-//               <div className="insta-content">
-//                 <Feed key={feed.id} feed={feed} />
-//             </div>
-//             ))}
-//         </div>
-//     );
-// }
 
 // export default InstagramFeed;
 import React, { useState, useEffect, useRef } from "react";
@@ -56,10 +5,10 @@ import "./instagramfeed.css";
 import Feed from './Feed'
 import axios from "axios";
 import Slider from "react-slick";
-// import "../slick/slick/slick.css";
-// import "../slick/slick/slick-theme.css";
+import "../slick/slick/slick.css";
+import "../slick/slick/slick-theme.css";
 
-const testToken = 'IGQVJXYTRQTVM0SHRhaXVjYnRFMjVmenhVak55dmI4eXBsdkNfLTVFV3BvU2RkYmxKRGdtR1hfUVlUQ2UtenpaYmp0ZAFRwU2daNW9CNUxvMjR5RnFFTzlmckdQeWpZALW9RMkFEdktqS1JwdDlmQ18tNQZDZD'
+const testToken = 'IGQVJXNzl1dS1laUowZA2hkWU92ZAl9KNkJuZAFFYZAFNleWxSMDdJVFVwbXRKQWI5bGtTRWEyNjBlOHBJcldqRVp3ZAlJwclN4MW43WUdMc3RvTmVJNmNSQ3pxYkhuanZAoWVFnQlFUODh3dnBKWngwSzJvNQZDZD'
 
 const InstagramFeed = ({ token, ...props }) => {
   const [feeds, setFeedsData] = useState([]);
@@ -102,7 +51,7 @@ const InstagramFeed = ({ token, ...props }) => {
 
     <Slider {...settings}>
       {feeds.map((feed) => (
-        <div classname = "slick-slide" key={feed.id}>
+        <div className = "slick-slide" key={feed.id}>
           <Feed feed={feed} />
         </div>
       ))}
--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -4,6 +4,7 @@ function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
+  const [editMode, setEditMode] = useState(false);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -34,39 +35,73 @@ function App() {
       .catch(error => console.error(error));
   }
 
+  const handleEditClick = () => {
+    setEditMode(true);
+  }
+
+  const handleSaveClick = () => {
+    setEditMode(false);
+    // Perform save action here
+  }
+
+  const handleDeleteClick = () => {
+    // Perform delete action here
+  }
+
   return (
-    <form onSubmit={handleFormSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={handleNameChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="description">Description:</label>
-        <textarea
-          id="description"
-          name="description"
-          value={description}
-          onChange={handleDescriptionChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="image">Image:</label>
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          onChange={handleFileChange}
-        />
-      </div>
-      <button type="submit">Upload</button>
-    </form>
+    <div>
+      {editMode ? (
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <textarea
+              id="description"
+              name="description"
+              value={description}
+              onChange={handleDescriptionChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="image">Image:</label>
+            <input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      ) : (
+        <div>
+          <div>
+            <label>Name:</label>
+            <span>{name}</span>
+          </div>
+          <div>
+            <label>Description:</label>
+            <span>{description}</span>
+          </div>
+          <div>
+            <label>Image:</label>
+            <img src={selectedFile ? URL.createObjectURL(selectedFile) : null} alt="Selected file" />
+          </div>
+          <button onClick={handleEditClick}>Edit</button>
+          <button onClick={handleDeleteClick}>Delete</button>
+        </div>
+      )}
+    </div>
   );
 }
 
