536b321bcaaf795206067a1128c084ab5ac83f72
sergioforwardslash <tequetin9@gmail.com>
2023-03-28 17:19:04 -0700

    login and register function

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
+import axios from "axios";
 
 const Login = () => {
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: "",
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/user/login", userInfo)
+      .then((response) => {
+        console.log("Loggedin");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserInfo((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="login section-padding">
       <div className="center">
         <h1>Login</h1>
-        <form method="post">
+        <form onSubmit={onSubmit}>
           <div className="input">
-            <input type="text" required />
+            <input
+              type="text"
+              name="username"
+              value={userInfo.username}
+              onChange={handleChange}
+              required
+            />
             <span></span>
             <label>Username</label>
           </div>
           <div className="input">
-            <input type="password" required />
+            <input
+              type="password"
+              name="password"
+              value={userInfo.password}
+              onChange={handleChange}
+              required
+            />
             <span></span>
             <label>Password</label>
           </div>
