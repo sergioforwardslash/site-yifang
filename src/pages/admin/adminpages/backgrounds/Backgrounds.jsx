46e666f27aad94ecae54b6beb66784754d12f2cf
Idannny <dannyivan6@outlook.com>
2023-04-03 16:09:01 -0700

    push to Ivan

--- src/pages/admin/adminpages/backgrounds/Backgrounds.jsx
+++ src/pages/admin/adminpages/backgrounds/Backgrounds.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import "./backgrounds.css"
+
+import { images } from '../../../../constants'
 
 const Backgrounds = () => {
-  return (
-    <div>Backgrounds</div>
-  )
-}
+  const [selectedBackground, setSelectedBackground] = useState('background.jpg');
 
-export default Backgrounds
+  const handleBackgroundChange = (background) => {
+    setSelectedBackground(background);
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${selectedBackground})`;
+  }, [selectedBackground]);
+
+  return (
+    <div className='bMain'>
+      <button onClick={() => handleBackgroundChange(images.background)}>Background 1</button>
+      {/* <button onClick={() => handleBackgroundChange('fruiteaLime.jpg')}>Background 2</button> */}
+      {/* <button onClick={() => handleBackgroundChange('menutea.png')}>Background 3</button> */}
+    </div>
+  );
+};
+
+export default Backgrounds;
