67b197b9ec1c175173e606626924a10dbc9aecdf
Idannny <dannyivan6@outlook.com>
2023-05-31 15:48:58 -0700

    Comittal

--- src/pages/admin/adminpages/managemenuitem/ManageMenuItem.jsx
+++ src/pages/admin/adminpages/managemenuitem/ManageMenuItem.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { AdminDashboardLinks } from '../../../../components';
-import './managemenuitem.css'
-import { menu } from '../../../../constants';
-// import { Sequelize } from 'sequelize'
+import './managemenuitem.css';
 
 const ManageMenuItem = () => {
   const [editMode, setEditMode] = useState(false);
@@ -12,19 +10,13 @@ const ManageMenuItem = () => {
     name: '',
     description: '',
     price: '',
-    
   });
-  const [menuItems, setMenuItems] = useState([]);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
-
-    const data = new FormData();
-    data.append('photo', event.target.files[0]);
-
     setFormData({
       ...formData,
-      photo: data,
+      photo: event.target.files[0],
     });
   };
 
@@ -37,73 +29,53 @@ const ManageMenuItem = () => {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    // const data = new FormData();
-    // data.append('name', formData.name);
-    // data.append('description', formData.description);
-    // data.append('price', formData.price);
-    // data.append('photo', selectedFile);
-  
-    axios.post('http://localhost:8000/admin/menuitem', formData)
+    const data = new FormData();
+    data.append('name', formData.name);
+    data.append('description', formData.description);
+    data.append('price', formData.price);
+    data.append('photo', selectedFile);
+
+    axios
+      .post('http://localhost:8000/admin/menuitem', data)
       .then((response) => {
-        console.log(formData);
-        // Append the new item to the menuItems array
-        // setMenuItems([...menuItems, response.data]);
+        console.log(response);
+        setFormData({
+          name: '',
+          description: '',
+          price: '',
+        });
+        setSelectedFile(null);
+        setEditMode(false);
+
+        // Fetch the newly created menu item from your API
+        axios
+          .get('http://localhost:8000/admin/menuitem')
+          .then((response) => {
+            const newItem = response.data; // Assuming the API response contains the new menu item object
+            // Update the frontend state with the new menu item
+            setFormData(newItem);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       })
-
-
-      .catch((error) => console.error(error));
-  
-    setEditMode(false);
-
-
-    setSelectedFile(null);
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   const handleEditClick = () => {
     setEditMode(true);
   };
 
-  // const handleUpload = async (event) => {
-  //   const file = event.target.files[0];
-  //   const formData = new FormData();
-  //   formData.append("menuItemImage", file);
-
-  //   try {
-  //     const response = await axios.post(
-  //       "http://localhost:8000/admin/menuitem",
-  //       formData
-  //     );
-  //     // const menuitem = response.data.path;  FOR USE 
-
-  //     console.log("Photo Uploaded");
-  //   } catch (error) {
-  //     console.log("Error uploading photo:", error);
-  //   }
-  // };
-
   const handleSaveClick = () => {
     handleFormSubmit();
-    setEditMode(false);
-    setFormData({
-      name: '',
-      description: '',
-      price: '',
-      photo: null,
-    });
-    setSelectedFile(null);
   };
-
-  useEffect(() => {
-    axios.get('http://localhost:8000/admin/menuitem')
-      .then((response) => {
-        setMenuItems(response.data);
-      })
-      .catch((error) => console.error(error));
-  }, []);
 
   const handleDeleteClick = () => {
     // Perform delete action here
   };
+
   return (
     <div className="admin-dashboard">
       <h1>
@@ -115,7 +87,7 @@ const ManageMenuItem = () => {
           <h2>Menu Item</h2>
           {editMode ? (
             <form onSubmit={handleFormSubmit}>
-              <div className="menuitem-name">
+              <div className="form-group">
                 <label htmlFor="name">Name:</label>
                 <input
                   type="text"
@@ -123,28 +95,31 @@ const ManageMenuItem = () => {
                   name="name"
                   value={formData.name}
                   onChange={handleChange}
+                  required
                 />
               </div>
-              <div className="menuitem-description">
+              <div className="form-group">
                 <label htmlFor="description">Description:</label>
                 <textarea
                   id="description"
                   name="description"
                   value={formData.description}
                   onChange={handleChange}
+                  required
                 />
               </div>
-              <div className="menuitem-price">
+              <div className="form-group">
                 <label htmlFor="price">Price: $</label>
                 <input
-                  type="text"
+                  type="number"
                   id="price"
                   name="price"
                   value={formData.price}
                   onChange={handleChange}
+                  required
                 />
               </div>
-              <div>
+              <div className="form-group">
                 <label htmlFor="image">Image:</label>
                 <input
                   type="file"
@@ -152,6 +127,7 @@ const ManageMenuItem = () => {
                   name="photo"
                   accept="image/*"
                   onChange={handleFileChange}
+                  required
                 />
               </div>
               <button type="submit">Save</button>
@@ -171,30 +147,20 @@ const ManageMenuItem = () => {
                 <span>{formData.price}</span>
               </div>
               <div>
-                <label htmlFor='file-upload'>Image:</label>
+                <label>Image:</label>
                 <img
                   src={selectedFile ? URL.createObjectURL(selectedFile) : null}
                   alt="Selected file"
                 />
-
               </div>
               <button onClick={handleEditClick}>Edit</button>
               <button onClick={handleDeleteClick}>Delete</button>
             </div>
           )}
         </div>
-        {menuItems.map((item) => (
-          <div key={item.id}>
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-            <p>{item.price}</p>
-            <img src={item.photo} alt={item.name} />
-          </div>
-        ))}
       </div>
     </div>
   );
-  }
-  
-  export default ManageMenuItem;
-  
+};
+
+export default ManageMenuItem;
