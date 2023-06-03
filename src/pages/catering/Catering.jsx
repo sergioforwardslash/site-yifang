8bf92564091e805275385c3d58f31ad5f76fde78
Idannny <dannyivan6@outlook.com>
2023-06-02 17:46:14 -0700

    live menu loaded to the front Order side

--- src/pages/catering/Catering.jsx
+++ src/pages/catering/Catering.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import './catering.css'
+import React, { useEffect, useState } from "react";
+import './catering.css';
+import axios from "axios";
 
-import { images } from '../../constants'
+import { images } from '../../constants';
 
 const Catering = () => {
+  
   return (
     <div className='catering section-padding' style={{ backgroundImage: `url(${images.background})`, backgroundSize: '100%' }}>
       <div className='wrapper catering-container'>
         <div className='catering-info' style={{ backgroundImage: `url(${images.catering})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}>
           <h1>Catering</h1>
           <p className='p__opensans'>Book us for you next event!</p>
-          <a href='/catering/order'><button className="custom-button">
+          <a href='/catering/order'>
+            <button className="custom-button">
               <span className="custom-span">
                 Contact
               </span>
@@ -25,11 +28,14 @@ const Catering = () => {
                   d="M0,0 C0,0 100,0 100,0 C100,0 100,100 100,100 C100,100 0,100 0,100 C0,100 0,0 0,0"
                 />
               </svg>
-            </button></a>
+            </button>
+          </a>
         </div>
+
+ 
       </div>
     </div>
-  )
+  );
 }
 
-export default Catering
+export default Catering;
